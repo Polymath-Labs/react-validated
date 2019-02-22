d7a07cad3068fb88b4d831633d5c3641f5fa82ab
Visar Gashi <visar@forte.fit>
2019-02-21 21:18:57 -0500

    Updated docker to work on windows environment, updated test portal to allow dynamic testing

--- dockerfile
+++ dockerfile
@@ -1,11 +1,16 @@
-FROM        node
+FROM        node:10
 
 # The base node image sets a very verbose log level.
 ENV         NPM_CONFIG_LOGLEVEL warn
 
-EXPOSE      3001
+RUN         apt-get update
+RUN         npm install -g npm create-react-app
 
-RUN         mkdir -p /react-validated
-WORKDIR     /react-validated
+RUN         echo 'export PATH=~/.local/bin:$PATH' >> ~/.bashrc
+
+EXPOSE      3000
+
+RUN         mkdir -p /root/react-validated/node_modules
+WORKDIR     /root/react-validated
 
 ENTRYPOINT  ["sleep", "2d"]
--- src/test-portal/App.js
+++ src/test-portal/App.js
@@ -1,40 +1,104 @@
 import React, { Component } from 'react'
 
-import { ValidatedInput, ValidatedForm } from 'react-validated';
+import { ValidatedInput, ValidatedForm } from '../index.lib';
 import 'bootstrap/dist/css/bootstrap.css'
 
 export default class App extends Component {
 
-  state = {
-    name: ''
+  constructor(props) {
+    super(props);
+
+    this.config = this.buildConfig();
+    this.state = {
+      text: '',
+      minLength: this.config.minLength
+    }
+  }
+
+  buildConfig = () => {
+    const config = {
+      minLength: 3
+    }
+    const args = window.location.search.split('&') || [];
+    for (let index = 0; index < args.length; index++) {
+      let keyValue = args[index].split('=');
+      switch (keyValue[0]) {
+        case 'minLength':
+        case '?minLength':
+          config.minLength = parseInt(keyValue[1], 10);
+          break;
+        default:
+        // nothing to do
+      }
+    }
+    return config;
   }
 
   onSave = () => {
     alert('Saved "' + this.state.name + '"')
   }
 
+  onChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  componen
+
   render() {
+
     return (
-      <div>
-        <ValidatedForm className="form-horizontal" onSubmit={this.onSave}>
-          <div className="form-group row">
-            <label htmlFor="name" className="col-sm-1 col-form-label">Name</label>
-            <div className="col-sm-4">
-              <ValidatedInput required min-length={{ params: 5 }}>
-                <input type="text" className="form-control" id="name" placeholder="Enter a Name"
-                  name="name" onChange={this.handleInputChange} value={this.state.name}
-                />
-              </ValidatedInput>
+      <div className="row">
+        <div className="col-8 offset-2 mt-3">
+          <div className="card">
+            <div className="card-header">
+              <h4 className="">Input Validator</h4>
+            </div>
+            <div className="card-body">
+              <ValidatedForm className="form-horizontal" onSubmit={this.onSave}>
+                <div className="form-group row">
+                  <label htmlFor="name" className="col-1 text-right col-form-label">Test text:</label>
+                  <div className="col-10">
+                    <ValidatedInput required min-length={{ params: this.config.minLength }}>
+                      <input type="text" className="form-control" id="testText" placeholder="Enter some text"
+                        name="text" onChange={this.onChange} value={this.state.name}
+                      />
+                    </ValidatedInput>
+                  </div>
+                  <div className="col-1">
+                    <button type="submit" className="btn btn-success">Save</button>
+                  </div>
+                </div>
+              </ValidatedForm>
             </div>
           </div>
-          <div className="row">
-            <div className="col-sm-10 text-right">
-              <button type="submit" className="btn btn-primary" data-background-color="orange">
-                Save
-              </button>
+
+          <div className="card mt-3">
+            <div className="card-header">
+              <h4 className="">Validator Config</h4>
+            </div>
+            <div className="card-body">
+              <form className="form-horizontal" action="/">
+                <div className="form-group row">
+                  <label htmlFor="minLength" className="col-2 text-right col-form-label">Min Length:</label>
+                  <div className="col-10">
+                    <input type="number" className="form-control" id="minLength" placeholder="Enter a min length"
+                      name="minLength" onChange={this.onChange} value={this.state.minLength}
+                    />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-10 offset-2">
+                    <button type="submit" className="btn btn-primary">Update Validation Config</button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
-        </ValidatedForm>
+        </div>
       </div>
     )
   }
